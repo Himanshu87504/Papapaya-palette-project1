875be081e4b31533f47ac916cea03500903e03c4
Himanshu <your-email@example.com>
2026-02-27 09:29:02 +0530

    add full project files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-amber-100 font-bold'>hello</h1>
+    </>
+  )
+}
+
+export default App
