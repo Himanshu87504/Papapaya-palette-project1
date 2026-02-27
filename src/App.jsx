dc187a6414725cf08d3a036dcad9c1a80ef3d0be
Himanshu <your-email@example.com>
2026-02-27 09:34:28 +0530

    your message

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1 className='bg-amber-100 font-bold'>hello</h1>
+      <h1 className='bg-amber-100 font-bold'>hello i am himanshu</h1>
     </>
   )
 }
